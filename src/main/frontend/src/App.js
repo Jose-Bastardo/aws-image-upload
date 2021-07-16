859e2fa03035356473e8f749ac382daaab31f48f
MajorZap <josebastardo123@gmail.com>
2021-07-16 17:24:08 -0400

    add frontend

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useCallback } from "react";
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+
+const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile")
+    .then(res => {
+      console.log(res);
+      const data = res.data;
+      setUserProfiles(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return userProfiles.map((userProfile, index) => {
+
+    return (
+      <div key={index}>
+        {userProfile.userProfileId ? (
+          <img
+            id={userProfile.userProfileId}
+            src={`http://localhost:8080/api/v1/user-profile/${userProfile.userProfileId}/image/download`} 
+            /> 
+        ) : null}
+        <br />
+        <br />
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        <Dropzone userProfileId={userProfile.userProfileId}/>
+        <br />
+      </div>
+    );
+  });
+};
+
+function Dropzone({ userProfileId }) {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+    formData,
+    {
+      headers: {
+        "Content_Type": "multipart/for-data"
+      }
+    }).then(() => {
+      console.log("File uploaded successfully")
+      const img = document.getElementById(userProfileId);
+      img.src=`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/download?` + new Date().getTime();
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfiles/>
+    </div>
+  );
+}
+
+export default App;
